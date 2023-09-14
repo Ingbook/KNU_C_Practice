fdc0892c91ada7e8a4f5d8c373bab01666fb5aef
이지성 <ydgeoubooke@gmail.com>
2023-09-14 11:28:13 +0900

    score

---
+++ 20230914/lecture7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int score = 0;
+	
+	scanf_s("%d", &score);
+
+	if (score > 100 || score < 0)
+		printf("잘못 입력하였습니다.");
+	else if (score > 90)
+		printf("A학점");
+	else if (score > 80)
+		printf("B학점");
+	else if (score > 70)
+		printf("C학점");
+	else if (score > 60)
+		printf("D학점");
+	else
+		printf("F학점");
+
+	return 0;
+}
