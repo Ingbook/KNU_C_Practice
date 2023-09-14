13334aa9a9ca171b45c0189c93a0fcbcaaa12584
이지성 <ydgeoubooke@gmail.com>
2023-09-14 12:22:07 +0900

    times table - while

---
+++ 20230914/lecture11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 2;
+	int j = 1;
+
+	while (i <= 9)
+	{
+		if (i == 5)
+		{
+			i++;
+			continue;
+		}
+		while (j <= 9)
+		{
+			printf("%d x %d = %d \n", i, j, i * j);
+			j++;
+		}
+		i++;
+		j = 1;
+		printf("\n");
+	}
+
+	return 0;
+}
