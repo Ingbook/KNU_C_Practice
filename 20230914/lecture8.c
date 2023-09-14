2184c40ac4ffa058b270a2f5235507d469f9b537
이지성 <ydgeoubooke@gmail.com>
2023-09-14 11:41:07 +0900

    score : switch

---
+++ 20230914/lecture8.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int score = 0;
+
+	scanf_s("%d", &score);
+	if (score > 100 || score < 0)
+	{
+		printf("잘못된 입력입니다.");
+		return 0;
+	}
+
+	int div = score / 10;
+
+	switch (div)
+	{
+	case 10:
+	case 9:
+		printf("A학점");
+		break;
+	case 8:
+		printf("B학점");
+		break;
+	case 7:
+		printf("C학점");
+		break;
+	case 6:
+		printf("D학점");
+		break;
+	default:
+		printf("F학점");
+	}
+
+	return 0;
+}
