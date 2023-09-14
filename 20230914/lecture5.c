a0333cbe885dcbd63153761e0fa360f4df49a55b
이지성 <ydgeoubooke@gmail.com>
2023-09-14 10:46:00 +0900

    leap year

---
+++ 20230914/lecture5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int year = 0;
+
+	scanf_s("%d", &year);
+	if ((year % 4 == 0 || year % 100 != 0) && year % 400 == 0)
+	{
+		printf("¿±≥‚¿‘¥œ¥Ÿ.");
+	}
+	else
+	{
+		printf("¿±≥‚¿Ã æ∆¥’¥œ¥Ÿ.");
+	}
+
+	return 0;
+}
